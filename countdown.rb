9d44b0a4f06596e7c6457ba7cb69c1907444bf17
Mickae Truman <mtruman@nyit.edu>
2019-02-14 04:51:42 +0000

    Automatically backed up by Learn

--- countdown.rb
+++ countdown.rb
@@ -1,16 +1,16 @@
 #write your code here
 def countdown
-def countdown(number)
-  while number > 0 
-  puts "#{number} SECOND(S)!"
-  number -= 1 
+def countdown(num)
+  while num > 0 
+  puts "#{num} SECOND(S)!"
+  num -= 1 
 end
  "HAPPY NEW YEAR!"
 end
 end
-def countdown_with_sleep(number)
-  while number > 0 
-  puts "#{number} SECOND(S)!"
+def countdown_with_sleep(num)
+  while num > 0 
+  puts "#{num} SECOND(S)!"
   sleep(1)
   num-=1 
 end
